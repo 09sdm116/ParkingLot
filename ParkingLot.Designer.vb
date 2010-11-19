3d375ee5488e76809d8a7a34e15e3474427410a2
Shad Miller <sdm12@sage.southhills.edu>
2010-11-19 14:57:46 -0500

    1st commit

---
+++ ParkingLot.Designer.vb
@@ -0,0 +1,241 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class ParkingLot
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnBack = New System.Windows.Forms.Button()
+        Me.btnSave = New System.Windows.Forms.Button()
+        Me.btnForward = New System.Windows.Forms.Button()
+        Me.btnGo = New System.Windows.Forms.Button()
+        Me.txtSpace = New System.Windows.Forms.TextBox()
+        Me.txtYear = New System.Windows.Forms.TextBox()
+        Me.txtMake = New System.Windows.Forms.TextBox()
+        Me.txtModel = New System.Windows.Forms.TextBox()
+        Me.txtGoTo = New System.Windows.Forms.TextBox()
+        Me.txtColor = New System.Windows.Forms.TextBox()
+        Me.txtLic = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'btnBack
+        '
+        Me.btnBack.Location = New System.Drawing.Point(87, 296)
+        Me.btnBack.Name = "btnBack"
+        Me.btnBack.Size = New System.Drawing.Size(75, 23)
+        Me.btnBack.TabIndex = 0
+        Me.btnBack.Text = "Back"
+        Me.btnBack.UseVisualStyleBackColor = True
+        '
+        'btnSave
+        '
+        Me.btnSave.Location = New System.Drawing.Point(204, 296)
+        Me.btnSave.Name = "btnSave"
+        Me.btnSave.Size = New System.Drawing.Size(75, 23)
+        Me.btnSave.TabIndex = 1
+        Me.btnSave.Text = "Save"
+        Me.btnSave.UseVisualStyleBackColor = True
+        '
+        'btnForward
+        '
+        Me.btnForward.Location = New System.Drawing.Point(320, 296)
+        Me.btnForward.Name = "btnForward"
+        Me.btnForward.Size = New System.Drawing.Size(75, 23)
+        Me.btnForward.TabIndex = 2
+        Me.btnForward.Text = "Foward"
+        Me.btnForward.UseVisualStyleBackColor = True
+        '
+        'btnGo
+        '
+        Me.btnGo.Location = New System.Drawing.Point(396, 191)
+        Me.btnGo.Name = "btnGo"
+        Me.btnGo.Size = New System.Drawing.Size(75, 23)
+        Me.btnGo.TabIndex = 3
+        Me.btnGo.Text = "Go"
+        Me.btnGo.UseVisualStyleBackColor = True
+        '
+        'txtSpace
+        '
+        Me.txtSpace.Location = New System.Drawing.Point(196, 27)
+        Me.txtSpace.Name = "txtSpace"
+        Me.txtSpace.Size = New System.Drawing.Size(100, 20)
+        Me.txtSpace.TabIndex = 4
+        '
+        'txtYear
+        '
+        Me.txtYear.Location = New System.Drawing.Point(196, 98)
+        Me.txtYear.Name = "txtYear"
+        Me.txtYear.Size = New System.Drawing.Size(100, 20)
+        Me.txtYear.TabIndex = 5
+        '
+        'txtMake
+        '
+        Me.txtMake.Location = New System.Drawing.Point(196, 134)
+        Me.txtMake.Name = "txtMake"
+        Me.txtMake.Size = New System.Drawing.Size(100, 20)
+        Me.txtMake.TabIndex = 6
+        '
+        'txtModel
+        '
+        Me.txtModel.Location = New System.Drawing.Point(196, 170)
+        Me.txtModel.Name = "txtModel"
+        Me.txtModel.Size = New System.Drawing.Size(100, 20)
+        Me.txtModel.TabIndex = 7
+        '
+        'txtGoTo
+        '
+        Me.txtGoTo.Location = New System.Drawing.Point(396, 153)
+        Me.txtGoTo.Name = "txtGoTo"
+        Me.txtGoTo.Size = New System.Drawing.Size(75, 20)
+        Me.txtGoTo.TabIndex = 8
+        '
+        'txtColor
+        '
+        Me.txtColor.Location = New System.Drawing.Point(196, 206)
+        Me.txtColor.Name = "txtColor"
+        Me.txtColor.Size = New System.Drawing.Size(100, 20)
+        Me.txtColor.TabIndex = 9
+        '
+        'txtLic
+        '
+        Me.txtLic.Location = New System.Drawing.Point(196, 242)
+        Me.txtLic.Name = "txtLic"
+        Me.txtLic.Size = New System.Drawing.Size(100, 20)
+        Me.txtLic.TabIndex = 10
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(142, 33)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(48, 13)
+        Me.Label1.TabIndex = 11
+        Me.Label1.Text = "Space #"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(164, 102)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(29, 13)
+        Me.Label2.TabIndex = 12
+        Me.Label2.Text = "Year"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(159, 138)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(34, 13)
+        Me.Label3.TabIndex = 13
+        Me.Label3.Text = "Make"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(157, 174)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(36, 13)
+        Me.Label4.TabIndex = 14
+        Me.Label4.Text = "Model"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(162, 210)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(31, 13)
+        Me.Label5.TabIndex = 15
+        Me.Label5.Text = "Color"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(149, 246)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(44, 13)
+        Me.Label6.TabIndex = 16
+        Me.Label6.Text = "License"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(397, 131)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(71, 13)
+        Me.Label7.TabIndex = 17
+        Me.Label7.Text = "Go To Space"
+        '
+        'ParkingLot
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(509, 383)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.txtLic)
+        Me.Controls.Add(Me.txtColor)
+        Me.Controls.Add(Me.txtGoTo)
+        Me.Controls.Add(Me.txtModel)
+        Me.Controls.Add(Me.txtMake)
+        Me.Controls.Add(Me.txtYear)
+        Me.Controls.Add(Me.txtSpace)
+        Me.Controls.Add(Me.btnGo)
+        Me.Controls.Add(Me.btnForward)
+        Me.Controls.Add(Me.btnSave)
+        Me.Controls.Add(Me.btnBack)
+        Me.Name = "ParkingLot"
+        Me.Text = "Parking Lot"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents btnBack As System.Windows.Forms.Button
+    Friend WithEvents btnSave As System.Windows.Forms.Button
+    Friend WithEvents btnForward As System.Windows.Forms.Button
+    Friend WithEvents btnGo As System.Windows.Forms.Button
+    Friend WithEvents txtSpace As System.Windows.Forms.TextBox
+    Friend WithEvents txtYear As System.Windows.Forms.TextBox
+    Friend WithEvents txtMake As System.Windows.Forms.TextBox
+    Friend WithEvents txtModel As System.Windows.Forms.TextBox
+    Friend WithEvents txtGoTo As System.Windows.Forms.TextBox
+    Friend WithEvents txtColor As System.Windows.Forms.TextBox
+    Friend WithEvents txtLic As System.Windows.Forms.TextBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents Label6 As System.Windows.Forms.Label
+    Friend WithEvents Label7 As System.Windows.Forms.Label
+
+End Class
---
+++ ParkingLot.vb
@@ -0,0 +1,62 @@
+﻿
+
+Structure Car
+
+    Dim year As String
+    Dim make As String
+    Dim model As String
+    Dim color As String
+    Dim license As String
+
+End Structure
+
+
+Public Class ParkingLot
+    Dim myCar(49) As Car
+    Private Sub btnSave_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnSave.Click
+
+        Dim sr As IO.StreamReader = IO.File.OpenText("..\..\CarFile.txt")
+        Dim i As Integer
+
+        Try
+            Do While (sr.Peek <> -1)
+
+                i += 1
+
+                myCar(i).year = sr.ReadLine
+                myCar(i).make = sr.ReadLine
+                myCar(i).model = sr.ReadLine
+                myCar(i).color = sr.ReadLine
+                myCar(i).license = sr.ReadLine
+
+                txtYear.Text = myCar(i).year
+                txtMake.Text = myCar(i).make
+                txtModel.Text = myCar(i).model
+                txtColor.Text = myCar(i).color
+                txtLic.Text = myCar(i).license
+
+            Loop
+
+        Catch ex As InvalidCastException
+            MessageBox.Show("Invalid Data")
+        Catch ex As Exception
+            MessageBox.Show("Error occurred")
+        End Try
+
+    End Sub
+
+    Private Sub btnBack_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnBack.Click
+
+    End Sub
+
+    Private Sub btnForward_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnForward.Click
+
+    End Sub
+
+    Private Sub btnGo_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnGo.Click
+
+        Dim intGo As Integer = CInt(txtGoTo.Text)
+
+
+    End Sub
+End Class
